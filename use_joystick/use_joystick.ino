6b0413c34dfd7c967fb5d877d3e24555d9932ae4
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-09-29 17:35:33 +0300

    Add code to collect joystick data

--- use_joystick/use_joystick.ino
+++ use_joystick/use_joystick.ino
@@ -1,9 +1,31 @@
+int xPin = A0;
+int yPin = A1;
+int buttonPin = 2;
+int xVal;
+int yVal;
+int buttonState;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(xPin, INPUT);
+  pinMode(yPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  xVal = analogRead(xPin);
+  yVal = analogRead(yPin);
 
+  buttonState = digitalRead(buttonPin);
+
+  Serial.print("X: ");
+  Serial.print(xVal);
+  Serial.print(" | Y: ");
+  Serial.print(yVal);
+  Serial.print(" | Button: ");
+  Serial.println(buttonState);
+
+  delay(100);
 }
